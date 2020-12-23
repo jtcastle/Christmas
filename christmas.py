1f4d8b687e279e3c312b2a79cc303cea834c91ad
jtcastle <56699410+jtcastle@users.noreply.github.com>
2020-12-22 17:22:27 -0800

    Done

---
+++ christmas.py
@@ -0,0 +1,38 @@
+from playsound import playsound
+import time
+
+print('$ pip install playsound')
+
+time.sleep(3)
+
+print("""
+This time of year (especially THIS year)""")
+time.sleep(3)
+print("""we all could use a little extra sunshine :)
+""")
+time.sleep(3)
+
+print('''
+
+              .     :     .
+            .  :    |    :  .
+             .  |   |   |  ,
+              \  |     |  /
+          .     ,-'"""`-.     .
+            "- /  __ __  \ -"
+              |==|  I  |==|
+        - --- | _`--^--'_ | --- -
+              |'`.     ,'`|
+            _- \  "---"  / -_
+          .     `-.___,-'     .
+              /  |     |  \
+            .'  |   |   |  `.
+               :    |    :
+              .     :     .
+              
+
+''')
+              
+
+playsound('pocket.mp3')
+
